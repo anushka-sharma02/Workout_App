c3b198ccb42b18926cc1e1afafd37c07d6a7dd27
anushka bhardwaj <anushka208bhardwaj@gmail.com>
2025-08-31 10:04:56 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/gym_exercises_app">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
